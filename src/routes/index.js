7d3b2aa0a387a2a1f82639fdd60db5d7b8eb7d8e
AbdullohXakimov <Abdullohh745@gmail.com>
2024-06-07 10:22:02 +0500

    The End

--- src/routes/index.js
+++ src/routes/index.js
@@ -34,6 +34,20 @@ const router = createRouter({
       component: () => import("../pages/Work.vue"),
     },
   ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      if (
+        to.matched.length > 1 &&
+        from.matched.length > 1 &&
+        to.matched[0].path === from.matched[0].path
+      ) {
+        return {};
+      }
+      return { top: 0 };
+    }
+  },
 });
 
 export default router;
